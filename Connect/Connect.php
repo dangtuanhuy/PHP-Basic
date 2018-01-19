aa05ec2115102bd747af4249f8fe213615a074f4
Unknown <heosumo27@gmail.com>
2018-01-19 11:38:33 +0700

    Sựa kết nối

--- Connect/Connect.php
+++ Connect/Connect.php
@@ -1,6 +1,6 @@
 <?php 
 $conn = 
-mysql_connect("localhost","root","mysql") or die(mysql_error());
+mysql_connect("localhost","root","") or die(mysql_error());
 mysql_set_charset('utf8');
 mysql_select_db("flowershop") or die("mysql can not find");
 ?>
